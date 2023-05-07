e3a8dc38b753e4bcb76071e59d18d3db6c131d10
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2023-05-07 19:16:23 +0200

    added content from API, narrowed amount of books rendered from the data and did some basic styling

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -8,17 +8,21 @@ const BookList = () => {
   useEffect(() => {
     fetch('https://project-express-api-mmopjfwcqq-lz.a.run.app/books/')
       .then((res) => res.json())
-      .then((data) => setBookList(data))
+      .then((data) => setBookList(data.slice(6, 27)))
       .catch((error) => console.error(error))
       .finally(() => console.log(bookList))
   }, [bookList])
 
   return (
-    <div>
+    <div className="books-container">
+      <h1>Books from Express API</h1>
+      <hr />
       {bookList.map((singleBook) => {
         return (
-          <div key={singleBook.bookID}>
-            {singleBook.title}
+          <div className="single-book-div" key={singleBook.bookID}>
+            <h2>&quot;{singleBook.title}&quot;</h2>
+            <h3>Written by: {singleBook.authors}</h3>
+            <p>Average rating: {singleBook.average_rating}</p>
           </div>
         )
       })}
